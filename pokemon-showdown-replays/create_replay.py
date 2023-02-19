c78a77da3aaea8ce4fb5e16066079e91e4865f20
eyalmen <dave.eyal@gmail.com>
2023-02-19 10:39:41 +0200

    Create the replay object from log

--- pokemon-showdown-replays/create_replay.py
+++ pokemon-showdown-replays/create_replay.py
@@ -1,5 +1,36 @@
 from datetime import datetime
 import math
+def create_replay_object(log: dict, show_full_damage: bool = False):
+    if not all(key in log for key in ('p1', 'p2', 'log', 'inputLog', 'roomid', 'format')):
+        raise ValueError("Invalid log object")
+    
+    if not show_full_damage:
+        log['log'] = hide_full_damage(log['log'])
+
+    log_as_string = "\n".join(log['log'])[:-1]
+
+    timestamp = get_unix_timestamp(log['timestamp'])
+    
+    private = {
+        'private': log['roomid'].count('-') == 3,
+        'password': "" if not log['roomid'].count('-') == 3 else log['roomid'].split('-')[3]
+    }
+
+    format = log['format']
+
+    if "|tier|" in log_as_string:
+        format = log_as_string.split("|tier|")[1].split("\n")[0]
+
+    return {
+        'id': log['roomid'],
+        'format': format,
+        'p1': log['p1'],
+        'p2': log['p2'],
+        'log': log_as_string,
+        'inputLog': log['inputLog'],
+        'timestamp': timestamp,
+        'private': private
+    }
 def hide_full_damage(log: list):
     for i, line in enumerate(log):
         if any(word in line for word in ("|damage|", "|-damage|", "|-heal|", "|switch|")):
